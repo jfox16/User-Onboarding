e2f54024b3ffe117849592a343cf5b70bb4777c7
Jonathan Fox <FoxJonathanP@gmail.com>
2021-02-25 14:31:59 -0800

    adding cypress tests

---
+++ cypress/integration/form_test.spec.js
@@ -0,0 +1,3 @@
+describe('User Onboarding App', () => {
+
+});
--- src/functions/createUser.js
+++ src/functions/createUser.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const BASE_URL = 'https://reqres.in/api/users';
 
 const createUser = (userData) => {
-  return axios.post(BASE_URL);
+  return axios.post(BASE_URL, userData);
 }
 
 export default createUser;

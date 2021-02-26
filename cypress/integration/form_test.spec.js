f58d9bad0621f6770cb4f2e119b894f1aaf4de75
Jonathan Fox <FoxJonathanP@gmail.com>
2021-02-25 17:20:12 -0800

    added cypress testing. Tests form inputs and error messages

--- cypress/integration/form_test.spec.js
+++ cypress/integration/form_test.spec.js
@@ -1,3 +1,53 @@
 describe('User Onboarding App', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
 
+  it('obeys the laws of the universe', () => {
+    expect(1 + 1).to.equal(2)
+    expect(1 + 1).not.to.equal(3);
+  });
+
+  const nameInput = () => cy.get('input[name=name]');
+  const emailInput = () => cy.get('input[name=email]');
+  const passwordInput = () => cy.get('input[name=password]');
+  const tosCheckbox = () => cy.get('input[name=tosAgreed]');
+  const submitButton = () => cy.get('button');
+
+  it('shows the proper form inputs', () => {
+    nameInput().should('exist');
+    emailInput().should('exist');
+    passwordInput().should('exist');
+    submitButton().should('exist');
+  });
+
+  it('can be filled and submitted', () => {
+    nameInput().clear().type('example name');
+    emailInput().clear().type('example@email.com');
+    passwordInput().clear().type('hunterhunter');
+    tosCheckbox().click();
+    submitButton().click();
+    cy.contains('example name');
+  });
+
+  it('shows proper error messages', () => {
+    nameInput().clear();
+    emailInput().clear();
+    submitButton().click();
+    cy.contains('Terms of Service must be agreed to');
+    tosCheckbox().click();
+    submitButton().click();
+    passwordInput().clear().type('asdf');
+    cy.contains('Password must be at least 8 characters long');
+    passwordInput().clear().type('asdf111111');
+    submitButton().click();
+    cy.contains('Password can only contain Latin letters');
+    passwordInput().clear().type('sadfasdfasdf');
+    submitButton().click();
+    cy.contains('No email provided');
+    emailInput().clear().type('example@email.com');
+    submitButton().click();
+    cy.contains('No name provided');
+    nameInput().clear().type('example name');
+  });
 });
